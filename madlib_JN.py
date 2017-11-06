ae604039c7a57191452a72cbef248a7c7558118e
jnicholson369 <33422384+jnicholson369@users.noreply.github.com>
2017-11-06 08:39:22 -0500

    Initial commit

---
+++ madlib_JN.py
@@ -0,0 +1,26 @@
+print("Write a verb")
+verb1 = input()
+
+print("Write a time of day")
+timeofday1 = input()
+
+print("Write a verb ending in ing")
+verb2 = input()
+
+print("Write a verb")
+verb3 = input()
+
+print("Write a noun")
+noun1 = input()
+
+print("Write an adjective")
+adjective1 = input()
+
+print("Write a number")
+number1 = input()
+### MAD LIB ###
+
+print("I always have wanted to" + verb1 + "."
+      "Every" + timeofday1 + " I dream of" + verb2 + "."
+      "Every night I" + verb3 + " in my" + noun1 + ", Which makes me feel" + adjective1 + ". "
+      "Last night I had" + number1 + "glasses of" +  )
